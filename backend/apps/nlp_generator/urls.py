25dfa804583a15ae989537cfd324575659a0d7e6
Mudasir ali <143800687+MDtech-code@users.noreply.github.com>
2025-05-08 13:49:21 +0500

    ending

--- backend/apps/nlp_generator/urls.py
+++ backend/apps/nlp_generator/urls.py
@@ -3,5 +3,5 @@ from .views import MCQGenerationView
 
 urlpatterns = [
     path('generate-mcq/', MCQGenerationView.as_view(), name='generate_mcq'),
-    # path('generate/ml/',MCQMLGenerationView.as_view(), name='generate_mcq_ml'),
+   
 ]
