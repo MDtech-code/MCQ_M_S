4bb3c6ee76254b469e1db714cfff048c31b1096e
Mudasir ali <143800687+MDtech-code@users.noreply.github.com>
2025-05-12 11:08:34 +0500

    minor improvement relatedt to docer configurations

--- backend/config/settings/development.py
+++ backend/config/settings/development.py
@@ -7,7 +7,11 @@ DEBUG = True
 # Debug toolbar
 INSTALLED_APPS += ['debug_toolbar']
 MIDDLEWARE.insert(0, 'debug_toolbar.middleware.DebugToolbarMiddleware')
-INTERNAL_IPS = ['127.0.0.1']
+# INTERNAL_IPS = ['127.0.0.1']
+import socket
+
+hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + ["127.0.0.1"]
 
 # Development-specific database
 DATABASES['default']['HOST'] = config('DB_HOST', default='localhost')
